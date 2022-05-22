6c5501863912dec1fb6995fe9f8b42c86b9fbadc
ChoiSangIl <wo566@naver.com>
2022-05-22 19:09:45 +0900

    feat: first commit

---
+++ src/main/java/com/kmong/project/KmongProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kmong.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmongProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmongProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmong/project/KmongProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmong.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmongProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
